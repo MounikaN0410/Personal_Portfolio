79de53f4f8ba45ca5e1ec04a9cc4c203c0ea2ac3
Mounika Nadimpalli <mounikanadimpalli@Mounikas-MacBook-Air.local>
2023-12-28 13:07:25 +0530

    Initial commit

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './intro.scss'
+
+export default function Intro() {
+  return (
+    <div className='intro' id='intro'>
+      <div className='left'>
+        <div className="imgContainer">
+          <img src='assets/Portfolio_Images/portfoliopicture.jpg' alt=""/>
+        </div>
+
+      </div>
+      <div className='right'>
+        
+      </div>
+      {/* intro */}
+      
+    </div>
+  )
+}
