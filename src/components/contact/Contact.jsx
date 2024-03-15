886341c2caa013b7e1ae738374fdabd2418045fa
MounikaN0410 <nmounika0410@gmail.com>
2024-03-15 18:39:13 -0400

    warning resolved

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,42 +1,42 @@
 import React from 'react'
 import './contact.scss'
-import emailjs from '@emailjs/browser';
-import { useState } from 'react';
+// import emailjs from '@emailjs/browser';
+// import { useState } from 'react';
 
 export default function Contact() {
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const [stateMessage, setStateMessage] = useState(null);
-  const sendEmail = (e) => {
-    e.persist();
-    e.preventDefault();
-    setIsSubmitting(true);
-    emailjs
-      .sendForm(
-        'service_9tbk9wf',
-        'template_w2pzvae',
-        e.target,
-        'n9Ba_xsmtd8AzzFMp'
-      )
-      .then(
-        (result) => {
-          setStateMessage('Message sent!');
-          setIsSubmitting(false);
-          setTimeout(() => {
-            setStateMessage(null);
-          }, 5000); // hide message after 5 seconds
-        },
-        (error) => {
-          setStateMessage('Something went wrong, please try again later');
-          setIsSubmitting(false);
-          setTimeout(() => {
-            setStateMessage(null);
-          }, 5000); // hide message after 5 seconds
-        }
-      );
+  // const [isSubmitting, setIsSubmitting] = useState(false);
+  // const [stateMessage, setStateMessage] = useState(null);
+  // const sendEmail = (e) => {
+  //   e.persist();
+  //   e.preventDefault();
+  //   setIsSubmitting(true);
+  //   emailjs
+  //     .sendForm(
+  //       'service_9tbk9wf',
+  //       'template_w2pzvae',
+  //       e.target,
+  //       'n9Ba_xsmtd8AzzFMp'
+  //     )
+  //     .then(
+  //       (result) => {
+  //         setStateMessage('Message sent!');
+  //         setIsSubmitting(false);
+  //         setTimeout(() => {
+  //           setStateMessage(null);
+  //         }, 5000); // hide message after 5 seconds
+  //       },
+  //       (error) => {
+  //         setStateMessage('Something went wrong, please try again later');
+  //         setIsSubmitting(false);
+  //         setTimeout(() => {
+  //           setStateMessage(null);
+  //         }, 5000); // hide message after 5 seconds
+  //       }
+  //     );
     
-    // Clears the form after sending the email
-    e.target.reset();
-  };
+  //   // Clears the form after sending the email
+  //   e.target.reset();
+  // };
 
 
 //   const sendEmail = async (e) => {
@@ -59,7 +59,7 @@ export default function Contact() {
       </div>
       <div className="right">
         <h2>Contact  </h2>
-        <form onSubmit={sendEmail}>
+        <form >
         <input type='text' placeholder='Email'/>
         <textarea placeholder='Message'/>
         <button >Submit</button>
